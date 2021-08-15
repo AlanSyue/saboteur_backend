a6d78776d2d4870120cc19022873bce147962234
alansyue <b123105@gmail.com>
2021-08-16 00:22:07 +0800

    feature: game function

--- app/src/models/rooms.models.ts
+++ app/src/models/rooms.models.ts
@@ -2,22 +2,30 @@ import { Schema, model } from 'mongoose';
 
 interface Room {
     owner: string;
+    is_start: boolean;
     max_players_number: number,
     chat_record: [object],
+    logs: [object],
     play_can_move: string;
     players: object;
-    board?: object;
+    board: object;
     cards: [number];
+    turn: [String];
+    end_block_cards: [number];
 }
 
 const schema = new Schema<Room>({
     owner: String,
+    is_start: {type: Boolean, default: false},
     max_players_number: Number,
     chat_record: Array,
+    logs: Array,
     play_can_move: String,
     players: Object,
-    board: {type: Object, default: null},
+    board: {type: Object, default: {}},
     cards: Array,
+    turn: Array,
+    end_block_cards: Array,
 });
 
 export const RoomModel = model<Room>('Rooms', schema);
